3e9e98aeba60f739b8e329b00c3b31be52d7b98e
ErikGunnarsson <ErikGunnarsson92@gmail.com>
2013-02-22 13:33:35 +0100

    test2

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -7,6 +7,7 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JTabbedPane;
 import javax.swing.JLabel;
 import java.awt.Color;
+import java.awt.Font;
 
 
 public class MainFrame extends JFrame {
@@ -42,9 +43,18 @@ public class MainFrame extends JFrame {
 		panel.setBackground(Color.GREEN);
 		panel.setBounds(0, 0, 434, 66);
 		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("iMat");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel.setBounds(10, 11, 56, 44);
+		panel.add(lblNewLabel);
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane.setBounds(147, 66, 287, 196);
 		contentPane.add(tabbedPane);
+		
+		JPanel panel_1 = new JPanel();
+		tabbedPane.addTab("New tab", null, panel_1, null);
 	}
 }
